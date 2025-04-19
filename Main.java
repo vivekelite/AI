bec4ca4e08543dfa124b25b20f1fa000b2d6ebe0
Harshal Patil <158513600+harshal-patil2005@users.noreply.github.com>
2025-04-19 15:26:27 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.print("The sun of numbers:");
+        int num1 = 7;
+        int num2 = 9;
+        int num3 = 1;
+        int sum = num1 + num2 + num3 ;
+        System.out.println(sum);
+    }
+}
